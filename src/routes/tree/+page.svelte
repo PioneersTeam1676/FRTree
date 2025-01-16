b85a73533d666c5fe415149cc9e557cd379f7adc
Bunalin <156369143+Bunalin@users.noreply.github.com>
2025-01-16 23:45:52 +0000

    Added a rough outline of the link tree page

---
+++ src/routes/tree/+page.svelte
@@ -0,0 +1,50 @@
+<div class="main">
+    <div class="link-divs">
+        <a class="link" href="https://team1676.com">come join 1676</a>
+        <p class="bg-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="link-divs">
+        <a class="link" href="https://team1676.com">come join 1676</a>
+        <p class="bg-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="link-divs">
+        <a class="link" href="https://team1676.com">come join 1676</a>
+        <p class="bg-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="link-divs">
+        <a class="link" href="https://team1676.com">come join 1676</a>
+        <p class="bg-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="link-divs">
+        <a class="link" href="https://team1676.com">come join 1676</a>
+        <p class="bg-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+</div>
+
+
+
+
+
+<style>
+
+.main{
+    background: pink;
+    margin-top: 100px;
+}
+
+.link-divs{
+    width: 70%;
+    height:50px;
+    background: purple;
+    margin: auto;
+    margin-top: 20px;
+}
+
+.link{
+    color: white;
+}
+
+.bg-info{
+    margin-top: 10px;
+}
+</style>
